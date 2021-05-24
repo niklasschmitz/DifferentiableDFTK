ac667d582749650fe0e49006c7035fb775021cb1
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-05-24 12:15:35 +0200

    add ForwardDiff, Zygote, ReverseDiff Bench

--- sandbox/silicon-stress/silicon.jl
+++ sandbox/silicon-stress/silicon.jl
@@ -31,9 +31,25 @@ function kinetic_energy(lattice, basis, ψ, occ)
     end
     E
 end
-E = kinetic_energy(lattice, basis, scfres.ψ, scfres.occupation)
+kinetic_energy(lattice) = kinetic_energy(lattice, basis, scfres.ψ, scfres.occupation)
+E = kinetic_energy(lattice)
 
 # diff E wrt lattice
-using ForwardDiff
-ForwardDiff.gradient(lattice -> kinetic_energy(lattice, basis, scfres.ψ, scfres.occupation), lattice)
 
+# works fine
+using ForwardDiff
+@time ForwardDiff.gradient(kinetic_energy, lattice) # 3.627630 seconds (5.99 M allocations: 363.981 MiB, 5.08% gc time, 98.69% compilation time)
+
+# works but long compile time and gives ComplexF64 results
+# hypothesis: slow compilation due to loop (and generator) unrolling
+using Zygote
+@time Zygote.gradient(kinetic_energy, lattice) # 61.094425 seconds (63.31 M allocations: 3.715 GiB, 3.85% gc time, 67.43% compilation time)
+
+# works fine
+using ReverseDiff
+@time ReverseDiff.gradient(kinetic_energy, lattice) # 5.409118 seconds (9.60 M allocations: 516.091 MiB, 14.61% gc time, 89.56% compilation time)
+
+using BenchmarkTools
+@btime ForwardDiff.gradient(kinetic_energy, lattice) #   3.152 ms (   7681 allocations:  11.00 MiB)
+@btime Zygote.gradient(kinetic_energy, lattice)      #  99.395 ms ( 358600 allocations: 129.91 MiB)
+@btime ReverseDiff.gradient(kinetic_energy, lattice) # 278.207 ms (4301696 allocations: 158.10 MiB)
