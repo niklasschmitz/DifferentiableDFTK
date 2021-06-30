4c5aa440eb0d076e23921b1ab4593688ac523140
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-06-30 12:05:29 +0200

    add working linsolve rrule

--- sandbox/1D/1D.jl
+++ sandbox/1D/1D.jl
@@ -85,7 +85,7 @@ function ChainRulesCore.rrule(config::RuleConfig{>:HasReverseMode}, ::typeof(nls
     return result, nlsolve_pullback
 end
 
-# 2.1 Try with NLsolve only (since we already defined its rrule above)
+# 2.1 Try with nesting NLsolve only (since we already defined its rrule above)
 
 let 
     blackbox = Vx -> begin
@@ -111,7 +111,7 @@ let
 end # looks promising
 
 
-# next step: implement an adjoint rule for linsolve or gmres
+# next step: implement an rrule for linsolve or gmres
 
 function ChainRulesCore.rrule(config::RuleConfig{>:HasReverseMode}, ::typeof(linsolve), A, b; kwargs...)
     result = linsolve(A, b; kwargs...)
@@ -120,37 +120,12 @@ function ChainRulesCore.rrule(config::RuleConfig{>:HasReverseMode}, ::typeof(lin
         Δx = Δresult[1]
         _, A_pullback = rrule_via_ad(config, A, x)
         Aᵀ(v) = A_pullback(v)[2]
-        b̄, _ = linsolve(Aᵀ, Δx; kwargs...) # discard convergence info
-        Ā = (-b̄) ⊗ x'
+        b̄, _ = linsolve(Aᵀ, -Δx; kwargs...) # discard convergence info
+        Ā = A_pullback(b̄)[1]  # w.r.t. A itself (implicitly closed-over variables)
         return (NoTangent(), Ā, b̄)
     end
     return (result, linsolve_pullback)
 end
-
-## check if the linsolve rrule works on a small linear system
-a = let
-    x = rand(10, 100)
-    x * x' + I
-end
-b = rand(10)
-
-linsolve(x->a*x, b)
-out, back = rrule(Zygote.ZygoteRuleConfig(), linsolve, x->a*x, b)
-df = unthunk(back(out)[2])
-df(out[1])
-back(out)
-
-out2, back2 = Zygote.pullback(A -> A \ b, a)
-back2(out2)
-out2
-
-out[1] ≈ out2
-back2(out2)
-back2(out[1])[1]*out[1]
-back2(out[1])[1]*out[1] ≈ df(out[1])
-
-# ^seems to work
-# TODO now try on the ode (use linsolve inside fp_frozen again)
 
 let 
     blackbox = Vx -> begin
@@ -158,88 +133,19 @@ let
         lin_op(u, u_frozen) = -lap(u) .+ @. Vx * u + α * u_frozen^2 * u
         # we solve the fixed-point u_frozen = g(u_frozen) where g(u_frozen) is the solution to the linear equation -u'' + Vu + α ufrozen^2 u = f
         function fp_frozen(u_frozen)
-            result = linsolve(u -> precond(lin_op(u, u_frozen)), precond(fx), tol=1e-14)
-            @show result
-            result[1]
+            linsolve(u -> precond(lin_op(u, u_frozen)), precond(fx), tol=1e-14)[1]
         end
         u_sol = nlsolve(u -> fp_frozen(u) - u, zeros(N), method = :anderson).zero
         sum(u_sol)
     end
-    # @time blackbox(Vx)
-    @time g1 = real(Zygote.gradient(blackbox, Vx)[1]) # LoadError: Need an adjoint for constructor var"#174#180"{Vector{Float64}, var"#lin_op#178"{Vector{Float64}}}. Gradient is of type LinearMaps.KroneckerMap{Float64, Tuple{LinearMaps.WrappedMap{Float64, Vector{Float64}}, LinearMaps.WrappedMap{Float64, Adjoint{Float64, Vector{Float64}}}}}
-    # g2 = ForwardDiff.gradient(blackbox, Vx)
-    # @time g3 = FiniteDiff.finite_difference_gradient(blackbox, Vx)
-    # @show g1[1:5]
-    # @show g3[1:5]
-    # @show sum(abs, g1 - g3) / length(g1)
-    # @btime $blackbox($x)
-    # @btime real(Zygote.gradient($blackbox, $Vx)[1])
-    # nothing
+    @time blackbox(Vx) # 0.636815 seconds (1.73 M allocations: 108.934 MiB, 7.03% gc time, 98.51% compilation time)
+    @time g1 = Zygote.gradient(blackbox, Vx)[1] # 4.534159 seconds (12.70 M allocations: 809.877 MiB, 6.34% gc time, 92.63% compilation time)
+    @time g2 = FiniteDiff.finite_difference_gradient(blackbox, Vx) # 2.504600 seconds (5.37 M allocations: 791.759 MiB, 7.66% gc time, 6.94% compilation time)
+    @show g1[1:5]
+    @show g2[1:5]
+    @show sum(abs, g1 - g2) / length(g1) # 1.8373434323529963e-6
+    @btime $blackbox($x) # 12.168 ms (37263 allocations: 5.48 MiB)
+    @btime Zygote.gradient($blackbox, $Vx)[1] # 293.429 ms (1954880 allocations: 132.05 MiB)
+    nothing
 end
-# ERROR: LoadError: Need an adjoint for constructor var"#174#180"{Vector{Float64}, var"#lin_op#178"{Vector{Float64}}}. Gradient is of type LinearMaps.KroneckerMap{Float64, Tuple{LinearMaps.WrappedMap{Float64, Vector{Float64}}, LinearMaps.WrappedMap{Float64, Adjoint{Float64, Vector{Float64}}}}}
-# Stacktrace:
-#   [1] error(s::String)
-#     @ Base ./error.jl:33
-#   [2] (::Zygote.Jnew{var"#174#180"{Vector{Float64}, var"#lin_op#178"{Vector{Float64}}}, Nothing, false})(Δ::LinearMaps.KroneckerMap{Float64, Tuple{LinearMaps.WrappedMap{Float64, Vector{Float64}}, LinearMaps.WrappedMap{Float64, Adjoint{Float64, Vector{Float64}}}}})
-#     @ Zygote ~/.julia/packages/Zygote/0da6K/src/lib/lib.jl:323
-#   [3] (::Zygote.var"#1748#back#209"{Zygote.Jnew{var"#174#180"{Vector{Float64}, var"#lin_op#178"{Vector{Float64}}}, Nothing, false}})(Δ::LinearMaps.KroneckerMap{Float64, Tuple{LinearMaps.WrappedMap{Float64, Vector{Float64}}, LinearMaps.WrappedMap{Float64, Adjoint{Float64, Vector{Float64}}}}})
-#     @ Zygote ~/.julia/packages/ZygoteRules/OjfTt/src/adjoint.jl:59
-#   [4] Pullback
-#     @ ~/Documents/cloud/sandbox/julia/dftk/DifferentiableDFTK/sandbox/1D/1D.jl:199 [inlined]
-#   [5] (::typeof(∂(λ)))(Δ::SubArray{Float64, 1, Matrix{Float64}, Tuple{Base.Slice{Base.OneTo{Int64}}, Int64}, true})
-#     @ Zygote ~/.julia/packages/Zygote/0da6K/src/compiler/interface2.jl:0
-#   [6] Pullback
-#     @ ~/Documents/cloud/sandbox/julia/dftk/DifferentiableDFTK/sandbox/1D/1D.jl:203 [inlined]
-#   [7] (::typeof(∂(λ)))(Δ::SubArray{Float64, 1, Matrix{Float64}, Tuple{Base.Slice{Base.OneTo{Int64}}, Int64}, true})
-#     @ Zygote ~/.julia/packages/Zygote/0da6K/src/compiler/interface2.jl:0
-#   [8] (::Zygote.var"#ad_pullback#41"{var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Tuple{Vector{Float64}}, typeof(∂(λ))})(Δ::SubArray{Float64, 1, Matrix{Float64}, Tuple{Base.Slice{Base.OneTo{Int64}}, Int64}, true})
-#     @ Zygote ~/.julia/packages/Zygote/0da6K/src/compiler/chainrules.jl:131
-#   [9] JT
-#     @ ~/Documents/cloud/sandbox/julia/dftk/DifferentiableDFTK/sandbox/1D/1D.jl:80 [inlined]
-#  [10] _unsafe_mul!(y::SubArray{Float64, 1, Matrix{Float64}, Tuple{Base.Slice{Base.OneTo{Int64}}, Int64}, true}, A::LinearMaps.FunctionMap{Float64, var"#JT#20"{Zygote.var"#ad_pullback#41"{var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Tuple{Vector{Float64}}, typeof(∂(λ))}}, Nothing}, x::SubArray{Float64, 1, Matrix{Float64}, Tuple{Base.Slice{Base.OneTo{Int64}}, Int64}, true})
-#     @ LinearMaps ~/.julia/packages/LinearMaps/r6i00/src/functionmap.jl:109
-#  [11] mul!
-#     @ ~/.julia/packages/LinearMaps/r6i00/src/LinearMaps.jl:147 [inlined]
-#  [12] expand!(arnoldi::IterativeSolvers.ArnoldiDecomp{Float64, LinearMaps.FunctionMap{Float64, var"#JT#20"{Zygote.var"#ad_pullback#41"{var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Tuple{Vector{Float64}}, typeof(∂(λ))}}, Nothing}}, Pl::Identity, Pr::Identity, k::Int64, Ax::Vector{Float64})
-#     @ IterativeSolvers ~/.julia/packages/IterativeSolvers/xIVNp/src/gmres.jl:287
-#  [13] iterate(g::IterativeSolvers.GMRESIterable{Identity, Identity, Vector{Float64}, FillArrays.Fill{Float64, 1, Tuple{Base.OneTo{Int64}}}, Vector{Float64}, IterativeSolvers.ArnoldiDecomp{Float64, LinearMaps.FunctionMap{Float64, var"#JT#20"{Zygote.var"#ad_pullback#41"{var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Tuple{Vector{Float64}}, typeof(∂(λ))}}, Nothing}}, IterativeSolvers.Residual{Float64, Float64}, Float64, IterativeSolvers.ModifiedGramSchmidt}, iteration::Int64)
-#     @ IterativeSolvers ~/.julia/packages/IterativeSolvers/xIVNp/src/gmres.jl:64
-#  [14] iterate
-#     @ ~/.julia/packages/IterativeSolvers/xIVNp/src/gmres.jl:59 [inlined]
-#  [15] iterate
-#     @ ./iterators.jl:159 [inlined]
-#  [16] iterate
-#     @ ./iterators.jl:158 [inlined]
-#  [17] gmres!(x::Vector{Float64}, A::LinearMaps.FunctionMap{Float64, var"#JT#20"{Zygote.var"#ad_pullback#41"{var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Tuple{Vector{Float64}}, typeof(∂(λ))}}, Nothing}, b::FillArrays.Fill{Float64, 1, Tuple{Base.OneTo{Int64}}}; Pl::Identity, Pr::Identity, abstol::Float64, reltol::Float64, restart::Int64, maxiter::Int64, log::Bool, initially_zero::Bool, verbose::Bool, orth_meth::IterativeSolvers.ModifiedGramSchmidt)
-#     @ IterativeSolvers ~/.julia/packages/IterativeSolvers/xIVNp/src/gmres.jl:207
-#  [18] gmres(A::LinearMaps.FunctionMap{Float64, var"#JT#20"{Zygote.var"#ad_pullback#41"{var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Tuple{Vector{Float64}}, typeof(∂(λ))}}, Nothing}, b::FillArrays.Fill{Float64, 1, Tuple{Base.OneTo{Int64}}}; kwargs::Base.Iterators.Pairs{Union{}, Union{}, Tuple{}, NamedTuple{(), Tuple{}}})
-#     @ IterativeSolvers ~/.julia/packages/IterativeSolvers/xIVNp/src/gmres.jl:143
-#  [19] gmres(A::LinearMaps.FunctionMap{Float64, var"#JT#20"{Zygote.var"#ad_pullback#41"{var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Tuple{Vector{Float64}}, typeof(∂(λ))}}, Nothing}, b::FillArrays.Fill{Float64, 1, Tuple{Base.OneTo{Int64}}})
-#     @ IterativeSolvers ~/.julia/packages/IterativeSolvers/xIVNp/src/gmres.jl:143
-#  [20] (::var"#nlsolve_pullback#19"{Zygote.ZygoteRuleConfig{Zygote.Context}, var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Vector{Float64}, NLsolve.SolverResults{Float64, Float64, Vector{Float64}, Vector{Float64}}})(Δresult::Base.RefValue{Any})
-#     @ Main ~/Documents/cloud/sandbox/julia/dftk/DifferentiableDFTK/sandbox/1D/1D.jl:83
-#  [21] ZBack
-#     @ ~/.julia/packages/Zygote/0da6K/src/compiler/chainrules.jl:91 [inlined]
-#  [22] (::Zygote.var"#kw_zpullback#40"{var"#nlsolve_pullback#19"{Zygote.ZygoteRuleConfig{Zygote.Context}, var"#175#181"{var"#fp_frozen#179"{var"#lin_op#178"{Vector{Float64}}}}, Vector{Float64}, NLsolve.SolverResults{Float64, Float64, Vector{Float64}, Vector{Float64}}}})(dy::Base.RefValue{Any})
-#     @ Zygote ~/.julia/packages/Zygote/0da6K/src/compiler/chainrules.jl:117
-#  [23] Pullback
-#     @ ~/Documents/cloud/sandbox/julia/dftk/DifferentiableDFTK/sandbox/1D/1D.jl:203 [inlined]
-#  [24] (::typeof(∂(#173)))(Δ::Float64)
-#     @ Zygote ~/.julia/packages/Zygote/0da6K/src/compiler/interface2.jl:0
-#  [25] (::Zygote.var"#46#47"{typeof(∂(#173))})(Δ::Float64)
-#     @ Zygote ~/.julia/packages/Zygote/0da6K/src/compiler/interface.jl:41
-#  [26] gradient(f::Function, args::Vector{Float64})
-#     @ Zygote ~/.julia/packages/Zygote/0da6K/src/compiler/interface.jl:59
-#  [27] macro expansion
-#     @ timing.jl:210 [inlined]
-#  [28] top-level scope
-#     @ ~/Documents/cloud/sandbox/julia/dftk/DifferentiableDFTK/sandbox/1D/1D.jl:207
-# in expression starting at /home/niku/Documents/cloud/sandbox/julia/dftk/DifferentiableDFTK/sandbox/1D/1D.jl:193
-
-# using ChainRulesCore
-# function ChainRulesCore.rrule(::Type{LinearMap}, f, M::Int)
-#     L = LinearMap(f, M)
-#     LinearMap_pullback(ΔL) = (NoTangent(), ΔL.f, NoTangent())
-#     return L, LinearMap_pullback
-# end
-# Zygote.refresh()
+# works! TODO: figure out speed improvement
