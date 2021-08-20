0334e89abf66c79ffed5c449cf23feec419524de
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-08-20 09:33:14 +0200

    add "AD without implicif diff" Zygote stack trace

--- sandbox/1D/1D.jl
+++ sandbox/1D/1D.jl
@@ -4,8 +4,8 @@ using BenchmarkTools
 using ChainRulesCore
 using FFTW
 using FiniteDiff
-using IterativeSolvers # gmres
-using KrylovKit # linsolve
+using IterativeSolvers: gmres # gmres
+using KrylovKit: linsolve # linsolve
 using LinearAlgebra
 using LinearMaps
 using NLsolve
@@ -31,40 +31,51 @@ const fx = f.(x)
 lap(u) = real(ifft(ωs.^2 .* fft(real(u)))) # computes u''
 precond(u) = real(ifft(fft(real(u)) ./ (1 .+ ωs .^ 2)))
 
-residual(u) = -lap(u) .+ @. Vx * u + α * u^3 - fx
-lin_op(u, u_frozen) = -lap(u) .+ @. Vx * u + α * u_frozen^2 * u
+# Goal: find e.g. d(sum(u*)) / dVx via AD
 
-# we solve the fixed-point u_frozen = g(u_frozen) where g(u_frozen) is the solution to the linear equation -u'' + Vu + α ufrozen^2 u = f
-function fp_frozen(u_frozen)
-    linsolve(u -> precond(lin_op(u, u_frozen)), precond(fx), tol=1e-14)[1]
+function blackbox_nlsolve_linsolve(Vx)
+    residual(u) = -lap(u) .+ @. Vx * u + α * u^3 - fx
+    lin_op(u, u_frozen) = -lap(u) .+ @. Vx * u + α * u_frozen^2 * u
+    # we solve the fixed-point u_frozen = g(u_frozen) where g(u_frozen) is the solution to the linear equation -u'' + Vu + α ufrozen^2 u = f
+    function fp_frozen(u_frozen)
+        f_inner(u) = precond(lin_op(u, u_frozen))
+        linsolve(f_inner, precond(fx), tol=1e-14)[1]
+    end
+    f_outer(u) = fp_frozen(u) - u
+    u_sol2 = nlsolve(f_outer, zeros(N), method=:anderson).zero
+    sum(u_sol2)
 end
 
-u_sol = nlsolve(u -> fp_frozen(u) - u, zeros(N), method = :anderson, show_trace=true).zero
-maximum(abs, residual(u_sol))
+@time blackbox_nlsolve_linsolve(Vx)
 
-## directly trying to solve without splitting doesn't work out numerically
-# nlsolve(u -> residual(u), zeros(N), method = :anderson, show_trace=true).zero
-
-#===#
-# TODO: find e.g. d(sum(u*)) / dVx via AD
-
-#===#
 # Approach 1: Direct black-box AD with Zygote (unrolling solvers in time)
 
-let 
-    blackbox = Vx -> begin
-        residual(u) = -lap(u) .+ @. Vx * u + α * u^3 - fx
-        lin_op(u, u_frozen) = -lap(u) .+ @. Vx * u + α * u_frozen^2 * u
-        # we solve the fixed-point u_frozen = g(u_frozen) where g(u_frozen) is the solution to the linear equation -u'' + Vu + α ufrozen^2 u = f
-        function fp_frozen(u_frozen)
-            linsolve(u -> precond(lin_op(u, u_frozen)), precond(fx), tol=1e-14)[1]
-        end
-        u_sol = nlsolve(u -> fp_frozen(u) - u, zeros(N), method = :anderson, show_trace=true).zero
-        sum(u_sol)
-    end
-    @time blackbox(Vx)
-    Zygote.gradient(blackbox, Vx) # ERROR: LoadError: Mutating arrays is not supported
-end
+# Zygote.gradient(blackbox_nlsolve_linsolve, Vx) # ERROR
+### the following error was extracted from the vs code debugger (otherwise caught by Zygote in interface2.jl)
+# MethodError: no method matching (::var"#3#4")(::Vector{Float64}, ::Vector{Float64})
+# Closest candidates are:
+#   (::var"#3#4")(::Any) at ~/Documents/cloud/sandbox/julia/dftk/DifferentiableDFTK/sandbox/1D/1D.jl:42 (method too new to be called from this world context.)
+# Stacktrace:
+#   [1] value!!(obj::NLSolversBase.NonDifferentiable{Vector{Float64}, Vector{Float64}}, F::Vector{Float64}, x::Vector{Float64})
+#     @ NLSolversBase ~/.julia/packages/NLSolversBase/geyh3/src/interface.jl:166
+#   [2] value!!(obj::NLSolversBase.NonDifferentiable{Vector{Float64}, Vector{Float64}}, x::Vector{Float64})
+#     @ NLSolversBase ~/.julia/packages/NLSolversBase/geyh3/src/interface.jl:163
+#   [3] anderson_(df::NLSolversBase.NonDifferentiable{Vector{Float64}, Vector{Float64}}, initial_x::Vector{Float64}, xtol::Float64, ftol::Float64, iterations::Int64, store_trace::Bool, show_trace::Bool, extended_trace::Bool, beta::Int64, aa_start::Int64, droptol::Float64, cache::NLsolve.AndersonCache{Vector{Float64}, Vector{Float64}, Vector{Vector{Float64}}, Vector{Float64}, Matrix{Float64}, Matrix{Float64}})
+#     @ NLsolve ~/.julia/packages/NLsolve/gJL1I/src/solvers/anderson.jl:73
+#   [4] anderson(df::NLSolversBase.NonDifferentiable{Vector{Float64}, Vector{Float64}}, initial_x::Vector{Float64}, xtol::Float64, ftol::Float64, iterations::Int64, store_trace::Bool, show_trace::Bool, extended_trace::Bool, beta::Int64, aa_start::Int64, droptol::Float64, cache::NLsolve.AndersonCache{Vector{Float64}, Vector{Float64}, Vector{Vector{Float64}}, Vector{Float64}, Matrix{Float64}, Matrix{Float64}})
+#     @ NLsolve ~/.julia/packages/NLsolve/gJL1I/src/solvers/anderson.jl:203
+#   [5] anderson(df::NLSolversBase.NonDifferentiable{Vector{Float64}, Vector{Float64}}, initial_x::Vector{Float64}, xtol::Float64, ftol::Float64, iterations::Int64, store_trace::Bool, show_trace::Bool, extended_trace::Bool, m::Int64, beta::Int64, aa_start::Int64, droptol::Float64)
+#     @ NLsolve ~/.julia/packages/NLsolve/gJL1I/src/solvers/anderson.jl:188
+#   [6] nlsolve(df::NLSolversBase.NonDifferentiable{Vector{Float64}, Vector{Float64}}, initial_x::Vector{Float64}; method::Symbol, xtol::Float64, ftol::Float64, iterations::Int64, store_trace::Bool, show_trace::Bool, extended_trace::Bool, linesearch::Static, linsolve::NLsolve.var"#29#31", factor::Float64, autoscale::Bool, m::Int64, beta::Int64, aa_start::Int64, droptol::Float64)
+#     @ NLsolve ~/.julia/packages/NLsolve/gJL1I/src/nlsolve/nlsolve.jl:30
+#   [7] (::NLsolve.var"#nlsolve##kw")(::NamedTuple{(:method, :show_trace), Tuple{Symbol, Bool}}, ::typeof(nlsolve), df::NLSolversBase.NonDifferentiable{Vector{Float64}, Vector{Float64}}, initial_x::Vector{Float64})
+#     @ NLsolve ~/.julia/packages/NLsolve/gJL1I/src/nlsolve/nlsolve.jl:18
+#   [8] nlsolve(f::var"#3#4", initial_x::Vector{Float64}; method::Symbol, autodiff::Symbol, inplace::Bool, kwargs::Base.Iterators.Pairs{Symbol, Bool, Tuple{Symbol}, NamedTuple{(:show_trace,), Tuple{Bool}}})
+#     @ NLsolve ~/.julia/packages/NLsolve/gJL1I/src/nlsolve/nlsolve.jl:52
+#   [9] (::NLsolve.var"#nlsolve##kw")(::NamedTuple{(:method, :show_trace), Tuple{Symbol, Bool}}, ::typeof(nlsolve), f::var"#3#4", initial_x::Vector{Float64})
+#     @ NLsolve ~/.julia/packages/NLsolve/gJL1I/src/nlsolve/nlsolve.jl:46
+#  [10] top-level scope
+
 
 #===#
 # Approach 2: Add the general rrule of nlsolve
@@ -87,31 +98,32 @@ end
 
 # 2.1 Try with nesting NLsolve only (since we already defined its rrule above)
 
-let 
-    blackbox = Vx -> begin
-        residual(u) = -lap(u) .+ @. Vx * u + α * u^3 - fx
-        lin_op(u, u_frozen) = -lap(u) .+ @. Vx * u + α * u_frozen^2 * u
-        # we solve the fixed-point u_frozen = g(u_frozen) where g(u_frozen) is the solution to the linear equation -u'' + Vu + α ufrozen^2 u = f
-        function fp_frozen(u_frozen)
-            nlsolve(u -> precond(lin_op(u, u_frozen)) - precond(fx), zeros(N), method = :anderson).zero
-        end
-        u_sol = nlsolve(u -> fp_frozen(u) - u, zeros(N), method = :anderson).zero
-        sum(u_sol)
+function blackbox_nlsolve_nlsolve(Vx)
+    residual(u) = -lap(u) .+ @. Vx * u + α * u^3 - fx
+    lin_op(u, u_frozen) = -lap(u) .+ @. Vx * u + α * u_frozen^2 * u
+    # we solve the fixed-point u_frozen = g(u_frozen) where g(u_frozen) is the solution to the linear equation -u'' + Vu + α ufrozen^2 u = f
+    function fp_frozen(u_frozen)
+        nlsolve(u -> precond(lin_op(u, u_frozen)) - precond(fx), zeros(N), method=:anderson).zero
     end
-    @time blackbox(Vx) # 0.605172 seconds (1.47 M allocations: 105.368 MiB, 10.58% gc time, 98.51% compilation time)
-    @time g1 = Zygote.gradient(blackbox, Vx)[1] # 10.130100 seconds (16.95 M allocations: 982.691 MiB, 5.67% gc time, 99.35% compilation time)
-    # g2 = ForwardDiff.gradient(blackbox, Vx)
-    @time g3 = FiniteDiff.finite_difference_gradient(blackbox, Vx) # 3.132538 seconds (4.94 M allocations: 846.075 MiB, 10.13% gc time, 14.61% compilation time)
+    f_outer(u) = fp_frozen(u) - u
+    u_sol2 = nlsolve(f_outer, zeros(N), method=:anderson).zero
+    sum(u_sol2)
+end
+
+let 
+    @time blackbox_nlsolve_nlsolve(Vx) # 0.605172 seconds (1.47 M allocations: 105.368 MiB, 10.58% gc time, 98.51% compilation time)
+    @time g1 = Zygote.gradient(blackbox_nlsolve_nlsolve, Vx)[1] # 10.130100 seconds (16.95 M allocations: 982.691 MiB, 5.67% gc time, 99.35% compilation time)
+    @time g2 = FiniteDiff.finite_difference_gradient(blackbox_nlsolve_nlsolve, Vx) # 3.132538 seconds (4.94 M allocations: 846.075 MiB, 10.13% gc time, 14.61% compilation time)
     @show g1[1:5]
-    @show g3[1:5]
-    @show sum(abs, g1 - g3) / length(g1) # 0.014476332143572825
-    @btime $blackbox($x) # 21.492 ms (41011 allocations: 8.33 MiB)
-    @btime Zygote.gradient($blackbox, $Vx)[1] # 28.739 ms (116532 allocations: 15.70 MiB)
+    @show g2[1:5]
+    @show sum(abs, g1 - g2) / length(g1) # 0.014476332143572825
+    @btime blackbox_nlsolve_nlsolve($x) # 21.492 ms (41011 allocations: 8.33 MiB)
+    @btime Zygote.gradient(blackbox_nlsolve_nlsolve, $Vx)[1] # 28.739 ms (116532 allocations: 15.70 MiB)
     nothing
-end # looks promising
+end # looks good
 
 
-# next step: implement an rrule for linsolve or gmres
+# next step: implement an rrule for linsolve
 
 function ChainRulesCore.rrule(config::RuleConfig{>:HasReverseMode}, ::typeof(linsolve), A, b; kwargs...)
     result = linsolve(A, b; kwargs...)
@@ -124,28 +136,49 @@ function ChainRulesCore.rrule(config::RuleConfig{>:HasReverseMode}, ::typeof(lin
         Ā = A_pullback(b̄)[1]  # w.r.t. A itself (implicitly closed-over variables)
         return (NoTangent(), Ā, b̄)
     end
-    return (result, linsolve_pullback)
+    return result, linsolve_pullback
 end
 
-let 
-    blackbox = Vx -> begin
-        residual(u) = -lap(u) .+ @. Vx * u + α * u^3 - fx
-        lin_op(u, u_frozen) = -lap(u) .+ @. Vx * u + α * u_frozen^2 * u
-        # we solve the fixed-point u_frozen = g(u_frozen) where g(u_frozen) is the solution to the linear equation -u'' + Vu + α ufrozen^2 u = f
-        function fp_frozen(u_frozen)
-            linsolve(u -> precond(lin_op(u, u_frozen)), precond(fx), tol=1e-14)[1]
-        end
-        u_sol = nlsolve(u -> fp_frozen(u) - u, zeros(N), method = :anderson).zero
-        sum(u_sol)
-    end
-    @time blackbox(Vx) # 0.636815 seconds (1.73 M allocations: 108.934 MiB, 7.03% gc time, 98.51% compilation time)
-    @time g1 = Zygote.gradient(blackbox, Vx)[1] # 4.534159 seconds (12.70 M allocations: 809.877 MiB, 6.34% gc time, 92.63% compilation time)
-    @time g2 = FiniteDiff.finite_difference_gradient(blackbox, Vx) # 2.504600 seconds (5.37 M allocations: 791.759 MiB, 7.66% gc time, 6.94% compilation time)
+let
+    @time blackbox_nlsolve_linsolve(Vx) # 0.636815 seconds (1.73 M allocations: 108.934 MiB, 7.03% gc time, 98.51% compilation time)
+    @time g1 = Zygote.gradient(blackbox_nlsolve_linsolve, Vx)[1] # 4.534159 seconds (12.70 M allocations: 809.877 MiB, 6.34% gc time, 92.63% compilation time)
+    @time g2 = FiniteDiff.finite_difference_gradient(blackbox_nlsolve_linsolve, Vx) # 2.504600 seconds (5.37 M allocations: 791.759 MiB, 7.66% gc time, 6.94% compilation time)
     @show g1[1:5]
     @show g2[1:5]
     @show sum(abs, g1 - g2) / length(g1) # 1.8373434323529963e-6
-    @btime $blackbox($x) # 12.168 ms (37263 allocations: 5.48 MiB)
-    @btime Zygote.gradient($blackbox, $Vx)[1] # 293.429 ms (1954880 allocations: 132.05 MiB)
+    @btime blackbox_nlsolve_linsolve($x) # 12.168 ms (37263 allocations: 5.48 MiB)
+    @btime Zygote.gradient(blackbox_nlsolve_linsolve, $Vx)[1] # 293.429 ms (1954880 allocations: 132.05 MiB)
     nothing
 end
 # works! TODO: figure out speed improvement
+
+# TODO alternatively without KrylovKit: implement rrule for IterativeSolvers.gmres
+
+function ChainRulesCore.rrule(config::RuleConfig{>:HasReverseMode}, ::typeof(gmres), A, b; kwargs...)
+    x = gmres(A, b; kwargs...)
+    function linsolve_pullback(Δx)
+        _, A_pullback = rrule_via_ad(config, A, x)
+        Aᵀ(v) = A_pullback(v)[2]
+        ∂b = gmres(Aᵀ, -Δx; kwargs...)
+        ∂A = A_pullback(∂b)[1]  # w.r.t. A itself (implicitly closed-over variables)
+        return (NoTangent(), ∂A, ∂b)
+    end
+    return x, linsolve_pullback
+end
+
+# function blackbox_nlsolve_gmres(Vx)
+#     residual(u) = -lap(u) .+ @. Vx * u + α * u^3 - fx
+#     lin_op(u, u_frozen) = -lap(u) .+ @. Vx * u + α * u_frozen^2 * u
+#     # we solve the fixed-point u_frozen = g(u_frozen) where g(u_frozen) is the solution to the linear equation -u'' + Vu + α ufrozen^2 u = f
+#     function fp_frozen(u_frozen)
+#         L = LinearMap(u -> precond(lin_op(u, u_frozen)), length(precond(fx)))
+#         gmres(L, precond(fx))
+#     end
+#     u_sol = nlsolve(u -> fp_frozen(u) - u, zeros(N), method = :anderson).zero
+#     sum(u_sol)
+# end
+
+# let 
+#     @time blackbox_nlsolve_gmres(Vx) # 0.636815 seconds (1.73 M allocations: 108.934 MiB, 7.03% gc time, 98.51% compilation time)
+#     @time g1 = Zygote.gradient(blackbox_nlsolve_gmres, Vx)[1]
+# end
